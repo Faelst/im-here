bdef8477a68829f93de2cf2c8a95e492a619ef62
Rafael Silverio <fael_st@hotmail.com>
2022-11-10 13:06:50 -0300

    add prettier forrmat

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,55 +1,96 @@
-import { useState } from "react";
-import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { Participant } from "../../components";
-import styles from "./styles";
+import { useState } from 'react'
+import {
+    Alert,
+    FlatList,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native'
+import { Participant } from '../../components'
+import styles from './styles'
 
 export function Home() {
-    const [participantName, setParticipantName] = useState("");
-    const [participants, setParticipants] = useState<string[]>([]);
+    const [participantName, setParticipantName] = useState('')
+    const [participants, setParticipants] = useState<string[]>([])
 
     const handleParticipantAdd = () => {
-        if (participantName.trim() === "") {
-            Alert.alert("Nome inválido", "Digite um nome para adicionar um participante");
-            return;
+        if (participantName.trim() === '') {
+            Alert.alert(
+                'Nome inválido',
+                'Digite um nome para adicionar um participante',
+            )
+            return
         }
 
         if (participants.includes(participantName)) {
-            Alert.alert('Participante já adicionado', 'Pedro já está na lista de participantes');
-            return;
+            Alert.alert(
+                'Participante já adicionado',
+                'Pedro já está na lista de participantes',
+            )
+            return
         }
 
-        setParticipants([...participants, participantName]);
-        setParticipantName("");
-    };
+        setParticipants([...participants, participantName])
+        setParticipantName('')
+    }
 
     const handleParticipantRemove = (name: string) => {
-        Alert.alert('Remover participante', `Deseja remover ${name} da lista?`, [{
-            text: 'Não',
-            style: 'cancel'
-        }, {
-            text: 'Sim',
-            onPress: () => {
-                const newList = participants.filter(participant => participant !== name);
-                setParticipants(newList);
-            }
-        }]);
-    };
+        Alert.alert('Remover participante', `Deseja remover ${name} da lista?`, [
+            {
+                text: 'Não',
+                style: 'cancel',
+            },
+            {
+                text: 'Sim',
+                onPress: () => {
+                    const newList = participants.filter(
+                        (participant) => participant !== name,
+                    )
+                    setParticipants(newList)
+                },
+            },
+        ])
+    }
 
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>Nome do evento</Text>
-            <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+            <Text style={styles.eventDate}>{new Date().toISOString()}</Text>
 
             <View style={styles.form}>
-                <TextInput style={styles.input} placeholder="Nome do participante..." placeholderTextColor="#6B6B6D" value={participantName} onChangeText={setParticipantName} />
-                <TouchableOpacity style={styles.btn} activeOpacity={0.7} onPress={handleParticipantAdd}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante..."
+                    placeholderTextColor="#6B6B6D"
+                    value={participantName}
+                    onChangeText={setParticipantName}
+                />
+                <TouchableOpacity
+                    style={styles.btn}
+                    activeOpacity={0.7}
+                    onPress={handleParticipantAdd}
+                >
                     <Text style={styles.btnText}>+</Text>
                 </TouchableOpacity>
             </View>
 
             <Text style={styles.participantsTitle}>Participantes</Text>
 
-            <FlatList showsVerticalScrollIndicator={false} data={participants} keyExtractor={item => item} renderItem={({ item }) => <Participant name={item} onRemove={handleParticipantRemove} />} ListEmptyComponent={<Text style={styles.emptyListText}>Ninguém chegou ainda no evento ? {'\n'}Adicione participantes a sua lista...</Text>} />
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={participants}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <Participant name={item} onRemove={handleParticipantRemove} />
+                )}
+                ListEmptyComponent={
+                    <Text style={styles.emptyListText}>
+                        Ninguém chegou ainda no evento ? {'\n'}Adicione participantes a sua
+                        lista...
+                    </Text>
+                }
+            />
         </View>
-    );
+    )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,60 +1,60 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 15,
-    },
-    eventName: {
-      color: '#FDFCFE',
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 40,
-    },
-    eventDate: {
-      color: '#6B6B6D',
-    },
-    input: {
-        flex:1,
-        height: 55,
-        backgroundColor: '#1F1E25',
-        borderRadius: 10,
-        color: '#FDFCFE',
-        padding: 15,
-        fontSize: 18,
-        marginRight: 15,
-    },
-    btn: {
-        width: 50,
-        height: 50,
-        borderRadius: 10,
-        backgroundColor: '#31CF67',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    btnText: {
-        color: '#FFF',
-        fontSize: 30,
-    },
-    form: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: 20,
-        marginBottom: 37
-    },
-    participantsTitle: {
-      color: '#6B6B6D',
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-    emptyListText: {
-      color: '#FFF',
-      fontSize: 14,
-      textAlign: 'center',
-      marginTop: 20,
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 15,
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 40,
+  },
+  eventDate: {
+    color: '#6B6B6D',
+  },
+  input: {
+    flex: 1,
+    height: 55,
+    backgroundColor: '#1F1E25',
+    borderRadius: 10,
+    color: '#FDFCFE',
+    padding: 15,
+    fontSize: 18,
+    marginRight: 15,
+  },
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: '#FFF',
+    fontSize: 30,
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 37,
+  },
+  participantsTitle: {
+    color: '#6B6B6D',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  emptyListText: {
+    color: '#FFF',
+    fontSize: 14,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+})
 
-  export default styles;
+export default styles
